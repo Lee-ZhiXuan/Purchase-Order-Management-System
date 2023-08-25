c0b37554c016b2fdf7b04c80a8d82132182c9610
stick-byran <91017539+stick-byran@users.noreply.github.com>
2023-08-25 10:36:59 +0800

    Add files via upload

---
+++ SalesObject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SalesObjects;
+
+/**
+ *
+ * @author bryan
+ */
+interface SalesObject {
+    
+    abstract public void View();
+    abstract public void Create();
+    abstract public void Delete();
+    abstract public void Edit();
+}
